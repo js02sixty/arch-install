ff228d87add39c9d1c3f35544a0c58cc1cbd5d0e
John Saba <js02sixty@gmail.com>
2017-02-19 14:03:33 -0500

    version 2

--- auto-arch-install.sh
+++ auto-arch-install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-##
-# use:
-# dd if=/dev/zero of=/dev/sda bs=512 count=1 conv=notrunc
-# to clean partition
-##
-ml=/etc/pacman.d/mirrorlist
-mv $ml $ml.bak
-curl -o $ml "https://www.archlinux.org/mirrorlist/?country=US&protocol=http&ip_version=4"
-sed -i 's/^#\(.*\)/\1/g' $ml
+echo "Enter username"
+read newuser
+echo "Enter password"
+read newpw
+echo "Enter hostname"
+read hname
 
 timedatectl set-ntp true
 
@@ -29,14 +26,13 @@ swapon /dev/vg_os/lv_swap
 
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4 /dev/vg_os/lv_root
-#mkfs.ext4 /dev/vg_os/lv_home
+mkfs.ext4 /dev/vg_os/lv_home
 mkfs.xfs /dev/sdb
 
 mount /dev/vg_os/lv_root /mnt
 mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 mkdir -p /mnt/home
-#mount /dev/vg_os/lv_home /mnt/home
 mount /dev/sdb /mnt/home
 
 pacstrap -i /mnt base base-devel
@@ -45,15 +41,26 @@ genfstab -U /mnt > /mnt/etc/fstab
 echo en_US.UTF-8 UTF-8 >> /mnt/etc/locale.gen
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 sed '/^HOOKS/s/block/block lvm2/' -i /mnt/etc/mkinitcpio.conf
+echo $hname > /mnt/etc/hostname
 
-##arch-chroot /mnt /bin/bash
 arch-chroot /mnt locale-gen
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
 arch-chroot /mnt hwclock --systohc --utc
 arch-chroot /mnt mkinitcpio -p linux
-arch-chroot /mnt pacman -S --noconfirm dosfstools networkmanager \
-vim xorg-server xorg-xinit xorg virtualbox-guest-modules virtualbox-guest-utils \
-zsh openssh firefox gnome gnome-tweak-tool
+arch-chroot /mnt pacman -S --noconfirm \
+	dosfstools \
+	virtualbox-guest-modules \
+	virtualbox-guest-utils \
+	networkmanager \
+	vim \
+	xorg-server \
+	xorg-server-utils \
+	xorg-xinit \
+	rxvt-unicode \
+	urxvt-perls \
+#	plasma-meta \
+	firefox
+
 arch-chroot /mnt bootctl --path=/boot install
 
 bentry=/mnt/boot/loader/entries/arch.conf
@@ -63,11 +70,16 @@ echo "initrd         /initramfs-linux.img" >> $bentry
 echo "options        root=/dev/vg_os/lv_root rw" >> $bentry
 
 ldrcfg=/mnt/boot/loader/loader.conf
-echo "timeout 2" >> $ldrcfg
+echo "timeout 1" >> $ldrcfg
 echo "default arch">> $ldrcfg
 
-#-servarch-chroot /mnt timedatectl set-ntp true
+#arch-chroot /mnt timedatectl set-ntp true
 arch-chroot /mnt timedatectl set-timezone America/New_York
-arch-chroot /mnt systemctl enable NetworkManager gdm vboxservice
+arch-chroot /mnt systemctl enable NetworkManager
+arch-chroot /mnt useradd -m -G wheel -s /bin/bash $newuser
+arch-chroot /mnt echo $newname:$newpw | chpasswd
+arch-chroot /mnt echo root:Security#321 | chpasswd
+sed '/^# %wheel ALL=(ALL) NOPASSWD: ALL/s/^#//' -i /mnt/etc/sudoers
 
 #reboot
+
