12a1f86b8141f816bbc188db1fff9e25c29c78f1
John Saba <js02sixty@gmail.com>
2018-10-07 01:40:57 -0400

    organized code

--- auto-arch-install.sh
+++ auto-arch-install.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
+## Personalization
 echo "Enter username"
 read newuser
-# echo "Enter password"
-# read newpw
+echo "Enter password"
+read newpw
 echo "Enter root password"
 read rpw
 echo "Enter hostname"
 read hname
 
+## Adjust Time
+timedatectl set-ntp true
+## Update Mirrorlist Sources
 ml=/etc/pacman.d/mirrorlist
 mv $ml $ml.bak
 curl -o $ml "https://www.archlinux.org/mirrorlist/?country=US&protocol=http&ip_version=4"
 sed -i 's/^#\(.*\)/\1/g' $ml
 
-timedatectl set-ntp true
-timedatectl set-timezone America/New_York
+## Remove old paritions
+for lvol in $(lvs|awk 'NR > 1 {print $1}')
+do
+    lvremove ${lvol} --force
+done
 
-# for v_partition in $(parted -s /dev/sda print|awk '/^ / {print $1}')
-# do
-#    parted -s /dev/sda rm ${v_partition}
-# done
+vgremove ${vgs | awk 'NR==2 {print $1}'} --force
+pvremove ${pvs | awk 'NR==2 {print $1}'} --force
 
+for part in $(parted -s /dev/sda print|awk '/^ / {print $1}')
+do
+    parted -s /dev/sda rm ${part}
+done
+
+## Partition Drive
 parted --script /dev/sda mklabel gpt
 parted --script /dev/sda mkpart ESP fat32 1MiB 513MiB
 parted --script /dev/sda set 1 boot on
@@ -42,29 +53,37 @@ mkfs.ext4 /dev/vg_os/lv_root
 mkfs.ext4 /dev/vg_os/lv_home
 
 mount /dev/vg_os/lv_root /mnt
-mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
 mkdir -p /mnt/home
 mount /dev/vg_os/lv_home /mnt/home
 
-pacstrap -i /mnt base base-devel \
+## Install Distro
+pacstrap -i /mnt \
+	base \
+	base-devel \
 	grub efibootmgr \
+	networkmanager
 	zsh \
 	vim \
 	xorg-server \
-	plasma-wayland-session \
-	plasma-meta \
 	firefox
+	
+## Set Time
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+arch-chroot /mnt hwclock --systohc --utc
 
-genfstab -U /mnt >> /mnt/etc/fstab
+## Set Locale
 echo en_US.UTF-8 UTF-8 >> /mnt/etc/locale.gen
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
+arch-chroot /mnt locale-gen
+
+## Configure OS
+genfstab -U /mnt >> /mnt/etc/fstab
 sed '/^HOOKS/s/block/block lvm2/' -i /mnt/etc/mkinitcpio.conf
 echo $hname > /mnt/etc/hostname
-arch-chroot /mnt locale-gen
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
-arch-chroot /mnt hwclock --systohc --utc
 arch-chroot /mnt mkinitcpio -p linux
+
 #arch-chroot /mnt pacman -S --noconfirm \
 #	zsh \
 #	dosfstools \
@@ -102,12 +121,21 @@ arch-chroot /mnt mkinitcpio -p linux
 # echo "timeout 1" >> $ldrcfg
 # echo "default arch">> $ldrcfg
 
+## Enable Services
 arch-chroot /mnt systemctl enable NetworkManager
-arch-chroot /mnt useradd -m -G wheel -s /bin/zsh $newuser
-#arch-chroot /mnt echo $newuser:$newpw | chpasswd
-arch-chroot /mnt echo root:$rpw | chpasswd
-sed '/^# %wheel ALL=(ALL) NOPASSWD: ALL/s/^#//' -i /mnt/etc/sudoers
+#arch-chroot /mnt systemctl enable gdm
 
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+## Set Permissions
+arch-chroot /mnt useradd -m -G wheel -s /bin/zsh $newuser
+arch-chroot /mnt echo $newuser:$newpw | chpasswd
+arch-chroot /mnt echo root:$rpw | chpasswd
+sed '/^# %wheel ALL=(ALL) NOPASSWD: ALL/ s/^#//' -i /mnt/etc/sudoers
+
+## Boot Loader
+sed 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=2/' -i /mnt/etc/default
+sed 's/part_gpt part_msdos/part_gpt part_msdos lvm/' -i /mnt/etc/default
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-#reboot
+
+reboot
+
